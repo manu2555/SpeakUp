b6b3211af2e2265c582d22b1bab939286f3a8249
Manju Soreng <manju@dhwaniris.com>
2025-02-18 13:06:52 +0530

    fixing yup issue

--- backend/src/types/yup.d.ts
+++ backend/src/types/yup.d.ts
@@ -1,3 +1,14 @@
+import { ObjectSchema, AnySchema, StringSchema, DateSchema, NumberSchema } from '@types/yup';
+
 declare module 'yup' {
-  export * from '@types/yup';
+  export interface Schema<T> extends AnySchema<T> {}
+  export interface StringSchema<T> extends StringSchema<T> {}
+  export interface NumberSchema<T> extends NumberSchema<T> {}
+  export interface DateSchema<T> extends DateSchema<T> {}
+  export interface ObjectSchema<T> extends ObjectSchema<T> {}
+  
+  export function object<T>(): ObjectSchema<T>;
+  export function string(): StringSchema<string>;
+  export function number(): NumberSchema<number>;
+  export function date(): DateSchema<Date>;
 } 
